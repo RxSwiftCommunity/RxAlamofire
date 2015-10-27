3ca641d820eadd25cb61f39456882d60014d13ea
Junior B <junior@bonto.ch>
2015-10-27 13:12:33 +0100

    Add of cold observables from convenience global functions

--- RxAlamofire/Source/RxAlamofire.swift
+++ RxAlamofire/Source/RxAlamofire.swift
@@ -14,6 +14,315 @@ import Foundation
 import Alamofire
 import RxSwift
 
+// MARK: Wrap of global functions
+
+/**
+    Returns an observalbe of a request using the shared manager instance for the specified URL request.
+    The request is started immediately.
+
+    - parameter URLRequest: The URL to create the request
+    - returns: The observalbe of `NSData` for the created request.
+*/
+public func rx_request(URLRequest: URLRequestConvertible) -> Observable<NSData> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.request(URLRequest.URLRequest)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        request.response() { request, response, data, error in
+            if let e = error {
+                observer.on(.Error(e as ErrorType))
+            } else {
+                if let d = data {
+                    if 200 ..< 300 ~= response?.statusCode ?? 0 {
+                        observer.onNext(d)
+                        observer.onComplete()
+                    } else {
+                        observer.onError(NSError(domain: "Wrong status code, expected 200 - 206, got \(response?.statusCode ?? -1)",
+                            code: -1,
+                            userInfo: nil))
+                    }
+                } else {
+                    observer.onError(error ?? NSError(domain: "Empty data received", code: -1, userInfo: nil))
+                }
+            }
+        }
+        
+        return AnonymousDisposable {
+            request.cancel()
+        }
+    }
+}
+
+/**
+    Returns an observalbe of a request (with all information) using the shared manager instance for the specified URL request.
+    The request is started immediately.
+
+    - parameter URLRequest: The URL to create the request
+    - returns: The observalbe of `(NSURLRequest, NSHTTPURLResponse, NSData)` for the created request.
+ */
+public func rx_requestFull(URLRequest: URLRequestConvertible) -> Observable<(NSURLRequest, NSHTTPURLResponse, NSData)> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.request(URLRequest.URLRequest)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        request.response() { request, response, data, error in
+            if let e = error {
+                observer.on(.Error(e as ErrorType))
+            } else {
+                if let d = data {
+                    if let res = response, req = request where 200 ..< 300 ~= response?.statusCode ?? 0 {
+                        observer.onNext((req, res, d))
+                        observer.onComplete()
+                    } else {
+                        observer.onError(NSError(domain: "Wrong status code, expected 200 - 206, got \(response?.statusCode ?? -1)",
+                            code: -1,
+                            userInfo: nil))
+                    }
+                } else {
+                    observer.onError(error ?? NSError(domain: "Empty data received", code: -1, userInfo: nil))
+                }
+            }
+        }
+        
+        return AnonymousDisposable {
+            request.cancel()
+        }
+    }
+}
+
+/**
+    Returns an observalbe of a request using the shared manager instance for the specified URL request.
+    The request is started immediately.
+
+    - parameter URLRequest: The URL to create the request
+    - paramenter ecoding: The string encoding to use
+    - returns: The observalbe of `String` for the created request.
+ */
+public func rx_requestString(URLRequest: URLRequestConvertible, encoding: NSStringEncoding? = nil) -> Observable<String> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.request(URLRequest.URLRequest)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        request.responseString(encoding: encoding) { responseData in
+            let result = responseData.result
+            let response = responseData.response
+            
+            switch result {
+            case .Success(let s):
+                if 200 ..< 300 ~= response?.statusCode ?? 0 {
+                    observer.onNext(s)
+                    observer.onComplete()
+                } else {
+                    observer.onError(NSError(domain: "Wrong status code, expected 200 - 206, got \(response?.statusCode ?? -1)",
+                        code: -1,
+                        userInfo: nil))
+                }
+            case .Failure(let e):
+                observer.onError(e)
+            }
+        }
+        
+        return AnonymousDisposable {
+            request.cancel()
+        }
+    }
+}
+
+/**
+    Returns an observalbe of a request using the shared manager instance for the specified URL request.
+    The request is started immediately.
+
+    - parameter URLRequest: The URL to create the request.
+    - parameter options: The JSON reading options.
+    - returns: The observalbe of `AnyObject` for the created request.
+ */
+public func rx_requestJSON(URLRequest: URLRequestConvertible, options: NSJSONReadingOptions = .AllowFragments) -> Observable<AnyObject> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.request(URLRequest.URLRequest)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        request.responseJSON(options: options) { responseData in
+            
+            let result = responseData.result
+            let response = responseData.response
+            
+            switch result {
+            case .Success(let d):
+                if 200 ..< 300 ~= response?.statusCode ?? 0 {
+                    observer.onNext(d)
+                    observer.onComplete()
+                } else {
+                    observer.onError(NSError(domain: "Wrong status code, expected 200 - 206, got \(response?.statusCode ?? -1)",
+                        code: -1,
+                        userInfo: nil))
+                }
+            case .Failure(let e):
+                observer.onError(e)
+            }
+            
+        }
+        
+        return AnonymousDisposable {
+            request.cancel()
+        }
+    }
+}
+
+/**
+    Returns an observalbe of a request using the shared manager instance to upload a specific file to a specified URL.
+    The request is started immediately.
+
+    - parameter URLRequest: The request object to start the upload.
+    - paramenter file: An instance of NSURL holding the information of the local file.
+    - returns: The observalbe of `AnyObject` for the created request.
+ */
+public func rx_upload(URLRequest: URLRequestConvertible, file: NSURL) -> Observable<(NSData?, RxProgess)> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.upload(URLRequest, file: file)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+
+        return handleProgress(request, observer: observer)
+    }
+}
+
+/**
+    Returns an observalbe of a request using the shared manager instance to upload any data to a specified URL.
+    The request is started immediately.
+
+    - parameter URLRequest: The request object to start the upload.
+    - paramenter data: An instance of NSData holdint the data to upload.
+    - returns: The observalbe of `AnyObject` for the created request.
+ */
+public func rx_upload(URLRequest: URLRequestConvertible, data: NSData) -> Observable<(NSData?, RxProgess)> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.upload(URLRequest, data: data)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        return handleProgress(request, observer: observer)
+    }
+}
+
+/**
+    Returns an observalbe of a request using the shared manager instance to upload any stream to a specified URL.
+    The request is started immediately.
+
+    - parameter URLRequest: The request object to start the upload.
+    - paramenter stream: The stream to upload.
+    - returns: The observalbe of `(NSData?, RxProgress)` for the created upload request.
+ */
+public func rx_upload(URLRequest: URLRequestConvertible, stream: NSInputStream) -> Observable<(NSData?, RxProgess)> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.upload(URLRequest, stream: stream)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        return handleProgress(request, observer: observer)
+    }
+}
+
+/**
+    Creates a download request using the shared manager instance for the specified URL request.
+    - parameter URLRequest:  The URL request.
+    - parameter destination: The closure used to determine the destination of the downloaded file.
+    - returns: The observalbe of `(NSData?, RxProgress)` for the created download request.
+ */
+public func rx_download(URLRequest: URLRequestConvertible, destination: Request.DownloadFileDestination) -> Observable<(NSData?, RxProgess)> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.download(URLRequest, destination: destination)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        return handleProgress(request, observer: observer)
+    }
+}
+
+// MARK: Resume Data
+
+/**
+    Creates a request using the shared manager instance for downloading from the resume data produced from a
+    previous request cancellation.
+
+    - parameter resumeData:  The resume data. This is an opaque data blob produced by `NSURLSessionDownloadTask`
+    when a task is cancelled. See `NSURLSession -downloadTaskWithResumeData:` for additional
+    information.
+    - parameter destination: The closure used to determine the destination of the downloaded file.
+    - returns: The observalbe of `(NSData?, RxProgress)` for the created download request.
+*/
+public func rx_download(resumeData data: NSData, destination: Request.DownloadFileDestination) -> Observable<(NSData?, RxProgess)> {
+    return create() { observer -> Disposable in
+        let request = Manager.sharedInstance.download(data, destination: destination)
+        
+        if Manager.sharedInstance.startRequestsImmediately == false {
+            request.resume()
+        }
+        
+        return handleProgress(request, observer: observer)
+    }
+}
+
+// MARK: Internal convenience
+internal func handleProgress(request: Request, observer: AnyObserver<(NSData?, RxProgess)>) -> Disposable {
+    var progress: RxProgess?
+    
+    request.response() { request, response, data, error in
+        if let e = error {
+            observer.on(.Error(e as ErrorType))
+        } else {
+            if let d = data {
+                if 200 ..< 300 ~= response?.statusCode ?? 0 {
+                    if let progress = progress {
+                        observer.onNext((d, progress))
+                        observer.onComplete()
+                    } else {
+                        observer.onNext((d, RxProgess.None))
+                        observer.onComplete()
+                    }
+                } else {
+                    observer.onError(NSError(domain: "Wrong status code, expected 200 - 206, got \(response?.statusCode ?? -1)",
+                        code: -1,
+                        userInfo: nil))
+                }
+            } else {
+                observer.onError(error ?? NSError(domain: "Empty data received", code: -1, userInfo: nil))
+            }
+        }
+    }
+    
+    request.progress() { bytesWritten, totalBytesWritten, totalBytesExpectedToWrite in
+        let p = RxProgess(bytesWritten: bytesWritten,
+            totalBytesWritten: totalBytesWritten,
+            totalBytesExpectedToWrite: totalBytesExpectedToWrite)
+        progress = p
+        observer.onNext((nil, p))
+    }
+    
+    return AnonymousDisposable {
+        request.cancel()
+    }
+}
+
 // MARK: Request - Common Response Handlers
 extension Request {
     
@@ -214,4 +523,45 @@ extension Request {
             }
         }
     }
+}
+
+// MARK: RxProgress
+public struct RxProgess {
+    let bytesWritten: Int64
+    let totalBytesWritten: Int64
+    let totalBytesExpectedToWrite: Int64
+    
+    public static let None = RxProgess(bytesWritten: 0, totalBytesWritten: 0, totalBytesExpectedToWrite: 0)
+    
+    init(bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        self.bytesWritten = bytesWritten
+        self.totalBytesWritten = totalBytesWritten
+        self.totalBytesExpectedToWrite = totalBytesExpectedToWrite
+    }
+}
+
+// MARK: Response Code
+enum RxResponse: Int {
+    case OK = 200
+    case Created = 201
+    case Accepted = 202
+    
+    case MovedPermanently = 301
+    case TemporaryRedirect = 307
+    
+    case BadRequest = 400
+    case Unauthorized = 401
+    case Forbidden = 403
+    case NotFound = 404
+    case MethodNotAllowed = 405
+    case NotAcceptable = 406
+    case RequestTimeout = 408
+    
+    case InternalServerError = 500
+    case BadGateway = 502
+    case ServiceUnavailable = 503
+    case GatewayTimeout = 504
+    case HTTPVersionNotSupported = 505
+    
+    case Unknown = 0
 }
