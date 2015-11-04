bbd2617b7a84ecc7de9d80efc2073853b4cb4382
Junior B <junior@bonto.ch>
2015-11-04 10:57:33 +0100

    Remove useless commented code

--- RxAlamofire/Source/RxAlamofire.swift
+++ RxAlamofire/Source/RxAlamofire.swift
@@ -917,31 +917,3 @@ public struct RxProgress {
         }
     }
 }
-
-// MARK: Response Code
-/*
-enum RxResponse: Int {
-    case OK = 200
-    case Created = 201
-    case Accepted = 202
-    
-    case MovedPermanently = 301
-    case TemporaryRedirect = 307
-    
-    case BadRequest = 400
-    case Unauthorized = 401
-    case Forbidden = 403
-    case NotFound = 404
-    case MethodNotAllowed = 405
-    case NotAcceptable = 406
-    case RequestTimeout = 408
-    
-    case InternalServerError = 500
-    case BadGateway = 502
-    case ServiceUnavailable = 503
-    case GatewayTimeout = 504
-    case HTTPVersionNotSupported = 505
-    
-    case Unknown = 0
-}
-*/
