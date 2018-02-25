6a9f37b42f587f01866035150cfd250ea47eb4d0
Kaden Wilkinson <wilkinson.kaden@gmail.com>
2018-02-25 12:25:39 -0700

    Fix SPM paths

--- Package.swift
+++ Package.swift
@@ -24,9 +24,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxAlamofire",
-            dependencies: ["RxSwift", "Alamofire"]),
+            dependencies: ["RxSwift", "Alamofire"],
+            path: "Sources",
+            exclude: ["Cocoa"]),
         .testTarget(
             name: "RxAlamofireTests",
-            dependencies: ["RxAlamofire", "RxSwift", "Alamofire"]),
+            dependencies: ["RxAlamofire", "RxSwift", "Alamofire"],
+            path: "RxAlamofire/RxAlamofireTests"),
     ]
 )
