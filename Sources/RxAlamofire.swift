d7db82168d5a8946bea582be5f3a5d1c8eaa1a16
Zsolt Varadi <vzsg@users.noreply.github.com>
2018-04-08 19:16:20 +0200

    Add upload(multipartFormData) extension to SessionManager

--- Sources/RxAlamofire.swift
+++ Sources/RxAlamofire.swift
@@ -704,6 +704,58 @@ extension Reactive where Base: SessionManager {
         }
     }
 
+    // MARK: Multipart upload
+
+    /**
+     Returns an observable of an `UploadRequest` using the `url`, `method` and `headers`,
+     and a body that contains `multipart/form-data` content encoded from the input data,
+     provided by the `multipartFormData` parameter.
+
+     It is important to understand the memory implications of uploading `MultipartFormData`. If the cummulative
+     payload is small, encoding the data in-memory and directly uploading to a server is the by far the most
+     efficient approach. However, if the payload is too large, encoding the data in-memory could cause your app to
+     be terminated. Larger payloads must first be written to disk using input and output streams to keep the memory
+     footprint low, then the data can be uploaded as a stream from the resulting file. Streaming from disk MUST be
+     used for larger payloads such as video content.
+
+     The `encodingMemoryThreshold` parameter allows Alamofire to automatically determine whether to encode in-memory
+     or stream from disk. If the content length of the `MultipartFormData` is below the `encodingMemoryThreshold`,
+     encoding takes place in-memory. If the content length exceeds the threshold, the data is streamed to disk
+     during the encoding process. Then the result is uploaded as data or as a stream depending on which encoding
+     technique was used.
+
+     - parameter multipartFormData:       The closure used to append body parts to the `MultipartFormData`.
+     - parameter encodingMemoryThreshold: The encoding memory threshold in bytes.
+                                          `multipartFormDataEncodingMemoryThreshold` by default.
+     - parameter url:                     The URL.
+     - parameter method:                  The HTTP method. `.post` by default.
+     - parameter headers:                 The HTTP headers. `nil` by default.
+    */
+    public func upload(multipartFormData data: @escaping (MultipartFormData) -> Void,
+                       usingThreshold encodingMemoryThreshold: UInt64 = SessionManager.multipartFormDataEncodingMemoryThreshold,
+                       to url: URLConvertible,
+                       method: HTTPMethod = .post,
+                       headers: HTTPHeaders = [:]) -> Observable<UploadRequest> {
+        let manager = base
+        return Observable.create { observer in
+            manager.upload(multipartFormData: data,
+                           usingThreshold: encodingMemoryThreshold,
+                           to: url,
+                           method: method,
+                           headers: headers) {
+                switch $0 {
+                case .failure(let error):
+                    observer.onError(error)
+                case .success(let request, _, _):
+                    observer.onNext(request)
+                    observer.onCompleted()
+                }
+            }
+
+            return Disposables.create()
+        }
+    }
+
     // MARK: Download
 
     /**
