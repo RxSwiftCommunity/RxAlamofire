074ee8bd1ced31697c3939a05078caaa88de02f5
Ivan Smolin <ivan.smolin@touchin.ru>
2019-01-29 16:33:12 +0300

    add watchOS carthage target

---
+++ RxAlamofire-watchOS/RxAlamofire.h
@@ -0,0 +1,19 @@
+//
+//  RxAlamofire-watchOS.h
+//  RxAlamofire-watchOS
+//
+//  Created by petropavel on 29/01/2019.
+//  Copyright © 2019 RxSwiftCommunity. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RxAlamofire-watchOS.
+FOUNDATION_EXPORT double RxAlamofire_watchOSVersionNumber;
+
+//! Project version string for RxAlamofire-watchOS.
+FOUNDATION_EXPORT const unsigned char RxAlamofire_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAlamofire_watchOS/PublicHeader.h>
+
+
---
+++ RxAlamofire/Pods/Target Support Files/RxSwift-watchOS/RxSwift-watchOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxSwift_watchOS : NSObject
+@end
+@implementation PodsDummy_RxSwift_watchOS
+@end
