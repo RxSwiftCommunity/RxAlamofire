3e22c2a5ddc83fbe34d3c5db11485ff3a68471de
Ryne Cheow <rynecheow@gmail.com>
2020-05-25 23:02:46 +0800

    fix(#176) - Fix cocoapods pipeline
    Signed-off-by: Ryne Cheow <rynecheow@gmail.com>

--- Examples/pod_install.sh
+++ Examples/pod_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
-export RELEASE_VERSION=$(git describe --abbrev=0 | tr -d '\n')
+export RELEASE_VERSION="${$(git describe --abbrev=0 | tr -d '\n'):1}"
 xcodegen
 pod install --repo-update
 open RxAlamofireDemo.xcworkspace
