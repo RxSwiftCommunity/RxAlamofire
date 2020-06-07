e2ae721efb30ff0ea5f59f79c47651070945b9ec
Ryne Cheow <rynecheow@gmail.com>
2020-06-07 12:07:23 +0800

    chore: Update dependencies
    Signed-off-by: Ryne Cheow <rynecheow@gmail.com>

---
+++ Examples/pod_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eo pipefail
+export RELEASE_VERSION="$(git describe --abbrev=0 | tr -d '\n')"
+RELEASE_VERSION=${RELEASE_VERSION:1}
+xcodegen
+pod update
+open RxAlamofireDemo.xcworkspace
